4ec7d86cb8a0f44e2ee6bda2e546f32f01490dd8
Fabrithegreat <fabrixio.rodrigo@gmail.com>
2022-11-09 14:05:22 +0100

    mobile menu update

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const closeBtn = document.querySelector('.close');
 const menu = document.querySelector("#menu");
+const body = document.body;
 
 hamburger.addEventListener('click', ()=> {
     menu.style.display = 'block';
     hamburger.style.display = 'none';
+    body.style.position = 'fixed';
 });
 closeBtn.addEventListener('click', ()=> {
     menu.style.display = 'none';
     hamburger.style.display = 'block';
+    body.style.position = 'unset';
 });
